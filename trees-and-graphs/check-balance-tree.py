b7a76099387821d961eef3f8862f2be1191ea33e
sazzad <sazzadhasan610@gmail.com>
2026-06-23 09:40:17 +0600

    update logic

--- trees-and-graphs/check-balance-tree.py
+++ trees-and-graphs/check-balance-tree.py
@@ -7,17 +7,14 @@ class TreeNode:
 def is_balanced(root):
     def dfs(node):
         if not node:
-            return 0
+            return [True,0]     # true mean , this is balanced and with height of the current node
+        
+        left,right = dfs(node.left),dfs(node.right)
+        isBalanced = left[0] and right[0] and abs(left[1]-right[1]) <=1
 
-        left = dfs(node.left)
-        right = dfs(node.right)
-
-        if left == -1 or right == -1 or abs(left - right) > 1:
-            return -1
-
-        return 1 + max(left, right)
-
-    return dfs(root) != -1
+        return [isBalanced,1+max(left[1],right[1])]
+    
+    return dfs(root)[0]
     
 
 root = TreeNode(1)
